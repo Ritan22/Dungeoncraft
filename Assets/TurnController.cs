7804bafd6f2c9ccca4061bb603bbb6f9c4f3cfcf
Leonardo Rizza <leo.r2002@live.it>
2024-03-19 09:43:30 +0100

    Primo commit

---
+++ Assets/TurnController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnController : MonoBehaviour
+{
+    public static bool turn;
+    public GameObject comands;
+    // Start is called before the first frame update
+    void Start()
+    {
+        turn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (turn == true){
+            comands.SetActive(true);
+        }
+        if (turn == false){
+            comands.SetActive(false);
+            Damage();
+            turn = true;
+        }
+    }
+
+    void Damage(){
+        Enemy.hp = Enemy.hp - 10;
+        Debug.Log(Enemy.hp);
+    }
+
+    public void AntiTurn(){
+        turn = false;
+    }
+}
